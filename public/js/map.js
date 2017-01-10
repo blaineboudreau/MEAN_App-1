2d6f9eabf76e9da93096684f572f1401c898442d
Blaine Boudreau <blaineboudreau@blaines-mbp.attlocal.net>
2017-01-10 13:58:53 -0600

    maps working

---
+++ public/js/map.js
@@ -0,0 +1,18 @@
+console.log("hi");
+
+function myMap() {
+  var mapCanvas = document.getElementById("map");
+  var myCenter = new google.maps.LatLng(40.7128,-74.0059);
+  var mapOptions = {center: myCenter, zoom: 5};
+  var map = new google.maps.Map(mapCanvas,mapOptions);
+  var marker = new google.maps.Marker({
+    position: myCenter,
+    icon: "http://buffetsingapore.sg/wp-content/uploads/2015/10/icon-clothing-store.svg"
+  });
+  marker.setMap(map);
+
+  var infowindow = new google.maps.InfoWindow({
+    content: "eTrendy"
+  });
+  infowindow.open(map,marker);
+};
