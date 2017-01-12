f0e63e618f099bf93eeffafa87377d37f2712980
Jessica Beardsley <jessicabeardsley@hotmail.com>
2017-01-11 18:14:51 -0700

    updating package.json and updating code for heroku

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var app = express();
 
 // 5. DATABASE
 // mongoose.connect('mongodb://localhost/mean_app');
-var mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/mean_app';
+var mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/mean_app';
 
 mongoose.connect(mongoUri);
 
